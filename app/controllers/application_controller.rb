38f9a9c206ab94abab432d53a66e22c4d717148b
alextaylor <alextaylor515@gmail.com>
2022-07-31 08:59:34 -0400

    Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,35 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  post "/chef/createaccount" do
+    
+  end
+  post "/user/createaccount" do
+
+  end
+  post "/login" do
+    User.find_by()
+  end
+  get "/chef/profile" do
+    
+  end
+  get "/user/home/:id" do
+    lat_range_upper = User.find(params[:id]).{latitude_upper:latitude+.05,longitude_upper:longitude+.05,
+        latitude_lower:latitude-.05,longitude_lower:longitude-.05}
+
+    Chef.where("longitude > ? and latitude",)
+  end
+  get "/user/profile" do
+    
+  end
+  patch "/chef/editprofile" do
+
+  end
+  patch "/user/editprofile" do
+
+  end
+  delete "/chef/post" do
+
   end
 
 end
